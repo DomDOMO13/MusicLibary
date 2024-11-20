b6b3db60e31396e1b1bf87235a87ac41791099a0
ANGEL <angel.player@outlook.com>
2024-11-20 22:30:37 +0700

    setup database

---
+++ database/AppContext.cs
@@ -0,0 +1,24 @@
+
+using Microsoft.EntityFrameworkCore;
+using musicplayer.Database;
+
+namespace musicplayer;
+
+public class AppContext : DbContext
+{
+
+	public DbSet<User> Users { get; set; }
+
+	private String dbpath;
+
+	public AppContext()
+	{
+		var folder = Environment.SpecialFolder.LocalApplicationData;
+		var path = Environment.GetFolderPath(folder);
+		this.dbpath = System.IO.Path.Join(path, "music.db");
+	}
+
+	protected override void OnConfiguring(DbContextOptionsBuilder options)
+			 => options.UseSqlite($"Data Source={this.dbpath}");
+
+}
